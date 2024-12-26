4653f53f463f5dfa9a23fdb2acec559942ad7f5e
pierre-alexis.deville <pierre-alexis.deville@ledger.fr>
2024-12-26 11:55:30 +0100

    delete useless prefetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 import CardErc7730 from "./address-abi-form";
 
 export default async function Home() {
-  void api.post.getLatest.prefetch();
-
   return (
     <HydrateClient>
       <div className="container m-auto flex h-screen max-w-2xl items-center justify-center p-4">
         <div>
-          <h1 className="mb-4 text-2xl font-bold">ERC7730 Jsn builder</h1>
+          <h1 className="mb-4 text-2xl font-bold">ERC7730 Json builder</h1>
           <CardErc7730 />
         </div>
       </div>
