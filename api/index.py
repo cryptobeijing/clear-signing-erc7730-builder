d7d404c12b6fb925dbe32045f2282276df057472
pierre-alexis.deville <pierre-alexis.deville@ledger.fr>
2024-12-27 15:09:11 +0100

    modify stuff again

--- api/index.py
+++ api/index.py
@@ -33,7 +33,7 @@ def run_erc7730(address: str, chain_id: int):
         etherscan_api_key = os.getenv("ETHERSCAN_API_KEY")
         env = os.environ.copy()
         env["ETHERSCAN_API_KEY"] = etherscan_api_key
-        env["XDG_CACHE_HOME"] = '/tmp'
+        env["XDG_CACHE_HOME"] = '/tmp/.cache'
         load_dotenv()
 
         result = generate_descriptor(
