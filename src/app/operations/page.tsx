74231694b4f707ba52983b47aeae75f52c253a80
pierre-alexis.deville <pierre-alexis.deville@ledger.fr>
2025-01-07 19:19:20 +0100

    change name

--- src/app/operations/page.tsx
+++ src/app/operations/page.tsx
@@ -8,7 +8,7 @@ export default async function Functions() {
     <HydrateClient>
       <div className="container mx-auto flex max-w-2xl flex-col justify-center p-4">
         <div className="flex w-full items-center justify-between">
-          <h1 className="text-2xl font-bold">Functions form</h1>
+          <h1 className="text-2xl font-bold">Operation form</h1>
           <ToolBox />
         </div>
         <BreadcrumbInfo />
