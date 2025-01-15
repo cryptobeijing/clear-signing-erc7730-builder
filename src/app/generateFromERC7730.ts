df9af2ccdb558d4875460bdbb8a60cf4915492a3
pierre-alexis.deville <pierre-alexis.deville@ledger.fr>
2025-01-15 16:12:44 +0100

    manage abi

---
+++ src/app/generateFromERC7730.ts
@@ -0,0 +1,35 @@
+import { type paths } from "~/generate/api-types";
+
+type GenerateBody =
+  paths["/api/py/generateERC7730"]["post"]["requestBody"]["content"]["application/json"];
+export type GenerateResponse =
+  paths["/api/py/generateERC7730"]["post"]["responses"]["200"]["content"]["application/json"];
+
+export default async function generateERC7730({
+  input,
+  inputType,
+}: {
+  inputType: "address" | "abi";
+  input: string;
+}): Promise<GenerateResponse | null> {
+  const body: GenerateBody = {
+    address: inputType === "address" ? input : undefined,
+    abi: inputType === "abi" ? input : undefined,
+  };
+
+  const response = await fetch("/api/py/generateERC7730", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (!response.ok) {
+    throw new Error(`API Error: ${response.statusText}`);
+  }
+
+  const data = (await response.json()) as GenerateResponse;
+
+  return data;
+}
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,6 +1,6 @@
 import { type paths } from "~/generate/api-types";
 
 export type Erc7730 =
-  paths["/api/py/generateFromAddress"]["post"]["responses"]["200"]["content"]["application/json"];
+  paths["/api/py/generateERC7730"]["post"]["responses"]["200"]["content"]["application/json"];
 
 export type Operation = Erc7730["display"]["formats"][string];
