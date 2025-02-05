505058d9316b9a1ef5fde7ae5e8d5313f6a9871a
pierre-alexis.deville <pierre-alexis.deville@ledger.fr>
2025-02-05 19:16:20 +0100

    add alpha tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default async function Home() {
     <HydrateClient>
       <div className="container m-auto flex h-screen max-w-2xl items-center justify-center p-4">
         <div>
-          <h1 className="mb-4 text-2xl font-bold">ERC7730 Json builder</h1>
+          <h1 className="mb-4 text-2xl font-bold">
+            ERC7730 Json builder <span className="text-red-500">Alpha</span>
+          </h1>
           <CardErc7730 />
         </div>
       </div>
